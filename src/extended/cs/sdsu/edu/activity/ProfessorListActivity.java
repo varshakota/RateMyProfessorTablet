681f6c2ff3607073a457de52b4a9949ee748e7fd
Varsha <varshakota@gmail.com>
2012-03-12 22:15:46 -0700

    initial commit

---
+++ src/extended/cs/sdsu/edu/activity/ProfessorListActivity.java
@@ -0,0 +1,13 @@
+package extended.cs.sdsu.edu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfessorListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
